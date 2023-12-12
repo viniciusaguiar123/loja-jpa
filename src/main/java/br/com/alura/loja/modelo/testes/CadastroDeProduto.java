a1a8fb732d536a70cd1913e4bf1d37f677e91e52
Vinicius <vini1068@gmail.com>
2023-12-11 21:48:39 -0300

    Mapeamentos

--- src/main/java/br/com/alura/loja/modelo/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/modelo/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.modelo.testes;
 
 import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -10,10 +11,7 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito Legal");
-        celular.setPreco(new BigDecimal("800"));
+        Produto celular = new Produto("Xiaomi Redmi", "Muito Legal", new BigDecimal("800"), Categoria.CELULARES);
 
         EntityManager em = JPAUtil.getEntityManager();
 
