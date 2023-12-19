17367fc31e82c5fe00225b1517a0844dbb4dd36f
Vinicius <vini1068@gmail.com>
2023-12-18 22:15:39 -0300

    Utilizando Named Queries

--- src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -36,8 +36,7 @@ public class ProdutoDAO {
     }
 
     public List<Produto> buscarPorNomeDaCategoria(String nome){
-        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
-        return em.createQuery(jpql, Produto.class)
+        return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class)
                 .setParameter("nome", nome)
                 .getResultList();
     }
