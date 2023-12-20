40e2004547942e2d1971d731ed28f7a937b24271
Vinicius <vini1068@gmail.com>
2023-12-19 22:54:29 -0300

    Mapeamento de chaves compostas

--- src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -5,23 +5,17 @@ import javax.persistence.*;
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
+    @EmbeddedId
+    private CategoriaId id;
 
     public Categoria() {
     }
 
     public Categoria(String nome) {
-        this.nome = nome;
+        this.id = new CategoriaId(nome, "xpto");
     }
 
     public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+        return this.id.getNome();
     }
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.testes;
 import br.com.alura.loja.dao.CategoriaDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.CategoriaId;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -43,6 +44,9 @@ public class CadastroDeProduto {
         produtoDAO.cadastrar(celular);
 
         em.getTransaction().commit();
+
+        em.find(Categoria.class, new CategoriaId("CELULARES", "xpto"));
+
         em.close();
     }
 }
