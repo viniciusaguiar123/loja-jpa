69717d39cfd07d2b9c2e51afa4020cb4ef1b05e8
Vinicius <vini1068@gmail.com>
2023-12-19 22:23:44 -0300

    Consultas com Criteria API

---
+++ src/main/java/br/com/alura/loja/testes/testeCriteria.java
@@ -0,0 +1,55 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ClienteDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Cliente;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class testeCriteria {
+
+    public static void main(String[] args) {
+        popularBancoDeDados();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        produtoDAO.buscarPorParametrosComCriteria(null, null, LocalDate.now());
+    }
+
+    private static void popularBancoDeDados() {
+        Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+        Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
+        Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
+
+        Cliente cliente = new Cliente("Rodrigo", "123456");
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDao = new ProdutoDAO(em);
+        CategoriaDAO categoriaDao = new CategoriaDAO(em);
+        ClienteDAO clienteDao = new ClienteDAO(em);
+
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        categoriaDao.cadastrar(videogames);
+        categoriaDao.cadastrar(informatica);
+
+        produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(videogame);
+        produtoDao.cadastrar(macbook);
+
+        clienteDao.cadastrar(cliente);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
